9804660852f1cf980b1c7174e378abd3f604878e
T-G <pronoygomes@yahoo.com>
2020-08-25 00:02:21 +0100

    added Jinja Template

--- app/views.py
+++ app/views.py
@@ -11,3 +11,57 @@ def index():
 @app.route("/about")
 def about():
     return render_template('public/about.html')
+
+
+@app.route("/jinja")
+def jinja():
+    
+    myname = 'Theo'
+    age = 40
+    
+    langs = ['Python', 'JavaScript', 'Bash', 'C', 'Ruby']
+
+    friends = {
+        'Tom' : 30,
+        'Amy' : 60,
+        'Tony' : 56,
+        'Clampad' : 23 
+    }
+
+    colors = ("Red", "Green")
+
+    cool = True
+
+    class GitRemote():
+        def __init__(self, name:str, description:str, url:str):
+            self.name = name
+            self.description = description
+            self.url = url
+
+        def pull(self):
+            return f"Pulling repo {self.name}"
+        
+        def clone(self):
+            return f"Cloning into {self.url}"
+
+    my_remote = GitRemote(
+        name = "Flask Jinja",
+        description = "Template design in Jinja",
+        url = "https://github.com/t-g/jinja.git"
+    )
+
+    def repeat(x:str, qty:int):
+        return x * qty
+
+    return render_template('public/jinja.html', 
+        myname = myname,
+        age = age,
+        langs = langs,
+        colors = colors,
+        cool = cool,
+        friends = friends,
+        GitRemote = GitRemote,
+        repeat = repeat,
+        my_remote = my_remote
+
+    )
